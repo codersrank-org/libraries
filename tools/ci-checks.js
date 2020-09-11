f4affea3128b300b9bd54b393db86ee5f118d294
Alim Giray Aytar <alimgiray@gmail.com>
2020-09-11 13:48:05 +0300

    change ci script for new structure

--- tools/ci-checks.js
+++ tools/ci-checks.js
@@ -4,31 +4,38 @@ fs.readFile( __dirname + '/../libraries.json', function (err, data) {
     throw err; 
   }
   data = JSON.parse(data)
-  let previousLibrary = ""
-  for (library in data) {
-    // Check alphabetical order
-    if (library.toLowerCase() < previousLibrary.toLowerCase()) {
-      console.log('ERROR: Libraries are not in alphabetical order. ' + previousLibrary + ' is before ' + library)
-      process.exit(1)
-    }
-    previousLibrary = library
 
-    // Check expected structure
-    if (!Array.isArray(data[library].imports) || data[library].imports.length === 0) {
-      console.log('ERROR: ' + library + '.imports must exist, must be an array and must have at least one element.')
+  let previousLanguage = ""
+  Object.keys(data).forEach(function(language) {
+    // Check alphabetical order of languages
+    if (language.toLowerCase() < previousLanguage.toLowerCase()) {
+      console.log('ERROR: Languages are not in alphabetical order. ' + previousLanguage + ' is before ' + language)
       process.exit(1)
     }
-    if (!Array.isArray(data[library].technologies)  || data[library].technologies.length === 0) {
-      console.log('ERROR: ' + library + '.technologies must exist, must be an array and must have at least one element')
-      process.exit(1)
+    previousLanguage = language
+
+    let previousLibrary = ""
+    for (library in data[language]){
+      // Check alphabetical order of libraries
+      if (library.toLowerCase() < previousLibrary.toLowerCase()) {
+        console.log('ERROR: Libraries are not in alphabetical order. ' + previousLibrary + ' is before ' + library + ' for ' + language)
+        process.exit(1)
+      }
+      previousLibrary = library
+
+      // Check expected structure
+      if (!Array.isArray(data[language][library].imports) || data[language][library].imports.length === 0) {
+        console.log('ERROR: ' + language + '.'  + library + '.imports must exist, must be an array and must have at least one element.')
+        process.exit(1)
+      }
+      if (!Array.isArray(data[language][library].technologies)  || data[language][library].technologies.length === 0) {
+        console.log('ERROR: ' + language + '.'  + library + '.technologies must exist, must be an array and must have at least one element')
+        process.exit(1)
+      }
+      if (typeof data[language][library].description === 'undefined' || data[language][library].description === '') {
+        console.log('ERROR: ' + language + '.' + library + '.description must exist and cannot be empty')
+        process.exit(1)
+      }
     }
-    if (typeof data[library].language === 'undefined' || data[library].language === '') {
-      console.log('ERROR: ' + library + '.language must exist and cannot be empty')
-      process.exit(1)
-    }
-    if (typeof data[library].description === 'undefined' || data[library].description === '') {
-      console.log('ERROR: ' + library + '.description must exist and cannot be empty')
-      process.exit(1)
-    }
-  }
+  })
 });
